4c402c3c3ec8afe90825f4aabec40b9f3bb79da1
Andrés <andres.cv@galileo.edu>
2016-07-16 17:58:05 -0600

    last?

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,6 +4,21 @@ $(function() {
         $('html, body').stop().animate({
             scrollTop: $($anchor.attr('href')).offset().top
         }, 1500, 'easeInOutExpo');
+        close();
         event.preventDefault();
     });
 });
+
+function close() {
+    if($("#toggle").hasClass("w3-show")) {
+        $("#toggle").removeClass("w3-show");
+    }
+}
+
+function toggle() {
+    if($("#toggle").hasClass("w3-show")) {
+        $("#toggle").removeClass("w3-show");
+    } else {
+        $("#toggle").addClass("w3-show");
+    }
+}
